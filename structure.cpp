cf0dc046255f6c553084f4652682a28551f7710b
Jazper Ramos <acer.ramos@yahoo.com>
2016-12-17 17:28:08 +0800

    Update structure.cpp

--- structure.cpp
+++ structure.cpp
@@ -6,10 +6,25 @@ struct Person{
 	int knees;
 	int shoulder;
 	int head;
+	
 };
-
 int main()
 {
+	Person person[5];
+	for (int i=0 ; i<5 ; i++)
+	{
+	cin >> person[i].toes;
+	cin >> person[i].knees;
+	cin >> person[i].shoulder;
+	cin >> person[i].head;
+	}
 	
+	for (int i=0 ; i<5 ; i++){
+	cout<<"Member: " << i+1<<endl;
+	cout << person[i].toes << endl;
+	cout << person[i].knees << endl;
+	cout << person[i].shoulder << endl;
+	cout << person[i].head << endl;
+	}
 	return 0;
 }
