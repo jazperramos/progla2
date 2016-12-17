59d6541050aed50dd5660ff88a3b428b4cf03585
Bryan Joshua B. Edejer <bryan.joshua.edejer@gmail.com>
2016-12-17 16:46:11 +0800

    Update structure.cpp

--- structure.cpp
+++ structure.cpp
@@ -11,6 +11,17 @@ struct Person
 
 int main ()
 {
+	Person person;
+	
+	cin >> person.toes;
+	cin >> person.knees;
+	cin >> person.shoulder;
+	cin >> person.head;
+	
+	cout << person.toes << endl;
+	cout << person.knees << endl;
+	cout << person.shoulder << endl;
+	cout << person.head << endl;	
 	
 	return 0;
 }
